873bb36669fc40f8fa3885ae38e146b1d9a80d61
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-04 00:34:38 +0530

    Finalized Jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Jumbotron from "../components/jumbotron";
+import { Particles } from "../components";
 
 export default function Home() {
   return (
@@ -7,6 +9,10 @@ export default function Home() {
         <title>bitwise</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Particles />
+      <div className="container mx-auto px-4">
+        <Jumbotron className="mt-20" />
+      </div>
     </div>
   );
 }
