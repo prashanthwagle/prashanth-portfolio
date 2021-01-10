ca99383318f73cc8fe387dad1fb8aa4235b4c698
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-11 02:16:12 +0530

    Added styles for icons

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -2,21 +2,61 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div style={{ position: "sticky", top: 0 }}>
+    <div style={{ position: "sticky", top: 0, zIndex: "2" }}>
       <div className="absolute top-0 h-full w-full  bg-darkViolet"></div>
       <nav className="h-12 w-full sticky">
         <ul className="list-none flex justify-evenly	">
           <li>
-            <i className="fas fa-home relative top-2/4 text-white cursor-pointer hover:text-blue-500"></i>
+            <i
+              onMouseEnter={(e) => {
+                e.target.classList.remove("text-white");
+                e.target.classList.add("text-darkGray");
+              }}
+              onMouseLeave={(e) => {
+                e.target.classList.remove("text-darkGray");
+                e.target.classList.add("text-white");
+              }}
+              className="fas fa-home relative top-2/4 text-white cursor-pointer hover:text-blue-500"
+            ></i>
           </li>
           <li>
-            <i className="fas fa-briefcase relative top-2/4 text-white cursor-pointer	hover:text-blue-500"></i>
+            <i
+              onMouseEnter={(e) => {
+                e.target.classList.remove("text-white");
+                e.target.classList.add("text-darkGray");
+              }}
+              onMouseLeave={(e) => {
+                e.target.classList.remove("text-darkGray");
+                e.target.classList.add("text-white");
+              }}
+              className="fas fa-briefcase relative top-2/4 text-white cursor-pointer	hover:text-blue-500"
+            ></i>
           </li>
           <li>
-            <i className="fas fa-language relative top-2/4 text-white cursor-pointer hover:text-blue-500"></i>
+            <i
+              onMouseEnter={(e) => {
+                e.target.classList.remove("text-white");
+                e.target.classList.add("text-darkGray");
+              }}
+              onMouseLeave={(e) => {
+                e.target.classList.remove("text-darkGray");
+                e.target.classList.add("text-white");
+              }}
+              className="fas fa-language relative top-2/4 text-white cursor-pointer hover:text-blue-500"
+            ></i>
           </li>
           <li>
-            <i className="fas fa-tools relative top-2/4	text-white cursor-pointer	hover:text-blue-500"></i>
+            <i
+              onMouseEnter={(e) => {
+                e.target.classList.remove("text-white");
+                e.target.classList.add("text-darkGray");
+              }}
+              onMouseLeave={(e) => {
+                e.target.classList.remove("text-darkGray");
+                e.target.classList.add("text-white");
+              }}
+              className="fas fa-tools relative top-2/4	text-white cursor-pointer	hover:text-blue-500"
+            ></i>
           </li>
         </ul>
       </nav>
