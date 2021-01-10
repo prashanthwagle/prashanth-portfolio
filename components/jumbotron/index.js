c83b163dde987f8970b0a7836b9e7dc05fafaca4
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-11 02:15:01 +0530

    Update index.js

--- components/jumbotron/index.js
+++ components/jumbotron/index.js
@@ -6,7 +6,7 @@ export default function Jumbotron({ ...restProps }) {
       <div className="mx-auto flex md:flex-row flex-col-reverse	 max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="md:text-left md:mt-0 mt-6 text-center my-auto">
           <h1 className="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
-            <div className="block text-indigo-600 xl:inline text-lightViolet">
+            <div className="block  xl:inline text-lightViolet">
               Hello friend!,
               <span className="animate-wave">👋🏻</span>
             </div>
