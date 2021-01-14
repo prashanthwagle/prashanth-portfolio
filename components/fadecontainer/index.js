5e092d51e56321f0875d18eeed948aea3b83f015
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-14 21:01:25 +0530

    Update index.js

--- components/fadecontainer/index.js
+++ components/fadecontainer/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 
-function Container({ fadeDir, children, ...restProps }) {
-  return (
-    <div className="container mx-auto mt-20 px-4" {...restProps}>
-      {fadeDir === "left" && <Fade left>{children}</Fade>}
-      {fadeDir === "right" && <Fade right>{children}</Fade>}
-    </div>
-  );
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.super = {};
+  }
+
+  render() {
+    const { fadeDir, children, ...restProps } = this.props;
+    return (
+      <div className="container mx-auto mt-20 px-4" {...restProps}>
+        {fadeDir === "left" && <Fade left>{children}</Fade>}
+        {fadeDir === "right" && <Fade right>{children}</Fade>}
+      </div>
+    );
+  }
 }
 
 export default Container;
+
+//Convert this to classComponent
