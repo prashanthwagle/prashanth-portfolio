76e030e1c24a5858f959cbbcd58a3a87cf199614
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-16 20:50:51 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Particles } from "./particles";
 export { default as Container } from "./fadecontainer";
 export { default as Timeline } from "./timeline";
+export { default as SkillShowcase } from "./skillshowcase";
