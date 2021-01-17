87f50aa4eaa2231080704db234e383edbea4638e
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-01-17 23:40:53 +0530

    Solved issue: Auto shrink of addressbar jumped background

--- components/particles/index.js
+++ components/particles/index.js
@@ -1,19 +1,37 @@
 import Particles from "react-particles-js";
+import { useEffect, useState } from "react";
 import config from "../../utils/particles.config.json";
+
 function ParticleBackground() {
   return (
-    <Particles
-      style={{
-        backgroundColor: "#5CDB94",
-        position: "fixed",
-        top: "0",
-        left: "0",
-        height: "100%",
-        width: "100%",
-        zIndex: "-1",
-      }}
-      params={config}
-    />
+    <>
+      <div
+        style={{
+          backgroundColor: "#5CDB94",
+          position: "fixed",
+          backgroundRepeat: "no-repeat !important",
+          backgroundSize: "cover !important",
+          height: "150%",
+          width: "100%",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          zIndex: "-3",
+        }}
+      ></div>
+      <Particles
+        style={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          zIndex: "-1",
+        }}
+        params={config}
+      />
+    </>
   );
 }
 
