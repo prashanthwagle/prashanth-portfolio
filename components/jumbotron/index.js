4735b1943d3981f005e01b59cbcde5eef5a9e49b
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-03-26 00:05:28 +0530

    Changed font weight

--- components/jumbotron/index.js
+++ components/jumbotron/index.js
@@ -6,19 +6,21 @@ export default function Jumbotron({ ...restProps }) {
       <div className="mx-auto flex md:flex-row flex-col-reverse	 max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="md:text-left md:mt-0 mt-6 text-center my-auto">
           <h1 className="text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
-            <div className="block xl:inline text-lightViolet">
+            <div className="block xl:inline font-extralight">
               Hello friend!,
               <span className="animate-wave">👋🏻</span>
             </div>
             <div
-              className="block text-lightViolet xl:inline"
+              className="block font-extralight xl:inline"
               style={{ display: "block" }}
             >
               I'm Prashanth
             </div>
           </h1>
-          <div className="text-2xl text-white font-extrabold ml-1 mt-3">
+          <div
+            className="text-2xl font-extrabold ml-1 mt-3"
             style={{ color: "#E03690" }}
+          >
             <Typewriter
               options={{
                 strings: [
