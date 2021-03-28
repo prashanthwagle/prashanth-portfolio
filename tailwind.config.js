280ad912a871575d0ed81351f517e6c41680ab70
Prashanth Wagle <prashanthwagle360@gmail.com>
2021-03-28 20:34:16 +0530

    Extending colors so that base colors can be used with extra colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    colors: {
-      deepBlack: "#1A1A1C",
-      darkGray: "#5CDB94",
-      darkViolet: "#E03690",
-      lightViolet: "black",
-      white: "#EDF4E0",
+    extend: {
+      colors: {
+        deepBlack: "#1A1A1C",
+        darkGray: "#5CDB94",
+        darkViolet: "#E03690",
+        lightViolet: "black",
+        white: "#EDF4E0",
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {
